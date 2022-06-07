81731d41b5299c654809584465daf59b60a6cc38
Kostiantyn <kostianaddur2008@gmail.com>
2022-06-08 00:33:26 +0300

    Second

--- Important/important_pkg.sh
+++ Important/important_pkg.sh
@@ -119,3 +119,90 @@ done
 echo ""
 echo -e "${ORANGE}Wait...${NC}"
 echo ""
+
+# Install VLC
+
+while true; do
+    echo "VLC - used to play videos "
+    echo " "
+    read -p "Do you wish to install VLC PKG?(yes or no) " yn
+    case $yn in
+        [Yy]* ) sudo pacman -S vlc; break;; 
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+echo ""
+echo -e "${ORANGE}Wait...${NC}"
+echo ""
+
+# Install GIMP
+
+while true; do
+    echo "GIMP - used to edit images "
+    echo " "
+    read -p "Do you wish to install GIMP PKG?(yes or no) " yn
+    case $yn in
+        [Yy]* ) sudo pacman -S gimp; break;; 
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+echo ""
+echo -e "${ORANGE}Wait...${NC}"
+echo ""
+
+# Install Flameshot
+
+while true; do
+    echo "Flameshot - used to take screenshots "
+    echo " "
+    read -p "Do you wish to install Flameshot PKG?(yes or no) " yn
+    case $yn in
+        [Yy]* ) sudo pacman -S flameshot; break;; 
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+echo ""
+echo -e "${ORANGE}Wait...${NC}"
+echo ""
+
+# Install OBS
+
+while true; do
+    echo "OBS - used to record and stream videos "
+    echo " "
+    read -p "Do you wish to install OBS PKG?(yes or no) " yn
+    case $yn in
+        [Yy]* ) sudo pacman -S obs-studio; break;; 
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+echo ""
+echo -e "${ORANGE}Wait...${NC}"
+echo ""
+
+# Install Standard Notes
+
+while true; do
+    echo "Standard Notes - used to write notes "
+    echo " "
+    read -p "Do you wish to install Standard Notes PKG?(yes or no) " yn
+    case $yn in
+        [Yy]* ) yay -S standardnotes-desktop; break;; 
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+echo ""
+echo -e "${ORANGE}Wait...${NC}"
+echo ""
+
+#
