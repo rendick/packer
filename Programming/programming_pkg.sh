d2eddeb3f5e24543bccb010fd17c9a1c40f7376d
Kostiantyn Naddur <kostianaddur@gmail.com>
2022-06-10 12:28:34 +0300

    Programming 1.6

--- Programming/programming_pkg.sh
+++ Programming/programming_pkg.sh
@@ -43,6 +43,23 @@ echo ""
 echo -e "${ORANGE}Wait...${NC}"
 echo ""
 
+# Install Hyper
+
+while true; do
+    echo "Hyper - best terminal"
+    echo " "
+    read -p "Do you wish to install Hyper?(yes or no) " yn
+    case $yn in
+        [Yy]* ) yay -S hyper; break;; 
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+echo ""
+echo -e "${ORANGE}Wait...${NC}"
+echo ""
+
 # Install Sublime Text
 
 while true; do
